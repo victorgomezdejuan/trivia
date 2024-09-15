07ba5306ca128ed9abe1092f609c3214afff2123
victor.gomez <victor.gomez@kurago.software>
2024-09-15 19:51:50 +0200

    Added one test that generates output in a text file just to see what it is printed in the console

--- Tests/GameTests.cs
+++ Tests/GameTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using Trivia;
 using Xunit;
 
 namespace Tests;
@@ -5,8 +8,33 @@ namespace Tests;
 public class GameTests
 {
     [Fact]
-    public void Test1()
+    public void GameWithTwoPlayers()
     {
-        Assert.True(true);
+        using var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        var game = new Game();
+
+        game.Add("Victor");
+        game.Add("Tao");
+
+        var random = new Random();
+        bool hasWon;
+
+        do
+        {
+            game.Roll(random.Next(5) + 1);
+
+            if (random.Next(9) == 7)
+            {
+                hasWon = game.WrongAnswer();
+            }
+            else
+            {
+                hasWon = game.WasCorrectlyAnswered();
+            }
+        } while (hasWon);
+
+        File.AppendAllText(@"C:\Training\Snapshot Testing\Tests\output.txt", stringWriter.ToString());
     }
 }
