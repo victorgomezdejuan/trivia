bfd8a3e8924930b0aef1d981c056775266cb6d39
victor.gomez <victor.gomez@kurago.software>
2024-09-21 20:52:46 +0200

    The test that uses snapshot testing now plays 100 games to increase real coverage and two more unit tests were added to unit tests two public methods

--- Tests/GameTests.cs
+++ Tests/GameTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using Trivia;
+using VerifyTests;
 using VerifyXunit;
 using Xunit;
 
@@ -10,19 +11,80 @@ namespace Tests;
 public class GameTests
 {
     [Fact]
-    public Task GameWithTwoPlayers()
+    public Task PlaySeveralGamesAndTestOutput()
     {
+        var gameOutputs = new string[100];
+
+        for (var seed = 0; seed < 100; seed++)
+        {
+            var output = PlayGame(seed);
+            gameOutputs[seed] = output; 
+        }
+
+        return Verifier.Verify(gameOutputs);
+    }
+
+    [Theory]
+    [InlineData(0, false)]
+    [InlineData(1, false)]
+    [InlineData(2, true)]
+    [InlineData(3, true)]
+    public void IsPlayableTest(int numberOfPlayers, bool expected)
+    {
+        var game = new Game();
+
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            game.Add($"Player {i}");
+        }
+
+        Assert.Equal(expected, game.IsPlayable());
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    public void HowManyPlayersTest(int numberOfPlayers)
+    {
+        var game = new Game();
+
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            game.Add($"Player {i}");
+        }
+
+        Assert.Equal(numberOfPlayers, game.HowManyPlayers());
+    }
+
+    private static string PlayGame(int seed)
+    {
+        var random = new Random(seed);
+
         using var stringWriter = new StringWriter();
         Console.SetOut(stringWriter);
 
         var game = new Game();
 
-        game.Add("Victor");
-        game.Add("Tao");
+        AddPlayers(game, random);
+        PlayGameUntilAPlayerWins(game, random);
 
-        var random = new Random(123);
+        return stringWriter.ToString();
+    }
+
+    private static void AddPlayers(Game game, Random random)
+    {
+        int numberOfPlayers = random.Next(5) + 1;
+
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            game.Add($"Player {i}");
+        }
+    }
+
+    private static void PlayGameUntilAPlayerWins(Game game, Random random)
+    {
         bool hasWon;
-
         do
         {
             game.Roll(random.Next(5) + 1);
@@ -36,7 +98,5 @@ public class GameTests
                 hasWon = game.WasCorrectlyAnswered();
             }
         } while (hasWon);
-
-        return Verifier.Verify(stringWriter.ToString());
     }
 }
